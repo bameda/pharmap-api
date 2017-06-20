53d6e72fae83cac9eead333257ae915480e4333e
David Barragán Merino <david.barragan@kaleidos.net>
2017-06-20 20:28:52 +0200

    Initialize storage.repositories

--- pharmap/storage/database.py
+++ pharmap/storage/database.py
@@ -1,4 +1,21 @@
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
+
+
+_db = None
+
+@property
+def db():
+    if not _db:
+        from apistar.main import get_current_app
+        from apistar.backends import SQLAlchemy
+        app = get_current_app()
+        _db = SQLAlchemy.build(settings=app.settings)
+    return _db
+
+
+__all__ = [
+    "Base",
+    "db"
+]
---
+++ pharmap/storage/repository/entities.py
@@ -0,0 +1,15 @@
+from apistar.backends import SQLAlchemy
+
+from pharmap.storage import Entity
+
+
+def create_entity(db: SQLAlchemy, name:str, description:str,
+                  lon:float, lat:float, is_active=True):
+    entity = Entity(
+        name=name,
+        description=description,
+        is_active=is_active,
+        point="POINT({} {})".format(lon, lat)
+    )
+    db.add(entity)
+    return entity
