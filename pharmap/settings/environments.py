2be91d7fa728fdb97f46aafe5e0aa3eaace08629
David Barragán Merino <david.barragan@kaleidos.net>
2017-06-20 20:28:13 +0200

    Add DB POOL_SIZE to the settings

--- pharmap/settings/environments.py
+++ pharmap/settings/environments.py
@@ -4,8 +4,9 @@ from apistar import environment, schema
 class Env(environment.Environment):
     properties = {
         "PHARMAP_DEBUG": schema.Boolean(default=False),
-        "PHARMAP_DATABASE_URL": schema.String(default="postgresql://:@localhost/pharmap")
+        "PHARMAP_DATABASE_URL": schema.String(default="postgresql://:@localhost/pharmap"),
         "PHARMAP_DATABASE_POOL_SIZE": schema.Integer(default=10)
     }
+
 
 env = Env()
